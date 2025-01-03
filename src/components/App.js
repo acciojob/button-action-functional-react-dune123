f081261c9180d5f835d9bd61b1794b94ea61e452
sraj39273@gmail.com <sraj39273@gmail.com>
2025-01-03 15:44:21 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,17 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+    const [data,setData]=useState(null);
+
+    const handleClick=()=>{
+      setData("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+    }
   return (
     <div id="main">
-      // Do not alter the main div
+        <p id="para">{data&&data}</p>
+        <button id="click" onClick={handleClick}>
+          click 
+        </button>
     </div>
   );
 }
